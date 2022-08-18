b91c1a2e96b06ce16461236ac0053c251569b174
aarnadlr <aaronadler@gmail.com>
2022-08-18 13:58:56 -0400

    Revise gif layout. Use Image.

--- pages/gif.tsx
+++ pages/gif.tsx
@@ -1,19 +1,37 @@
-import Image from 'next/image'
+import Image from 'next/image';
 
-const Gif = ()=>{
-  return(
-    <div>
-    <Image src="/static/giphy.gif" width='300' height="270" alt='gif' />
-    <style jsx>{`
-div{
-  height: 100vh;
-  background: black;
-  display: grid;
-  place-items: center;
-}
-`}</style>
-    </div>
-  )
-}
+const Gif = () => {
+  return (
+    <main>
+      <div>
+        <Image
+          layout="responsive"
+          src="/static/giphy.gif"
+          width="600"
+          height="500"
+          alt="gif"
+        />
+        
+      </div>
+      <style jsx>{`
+          //mobile
 
-export default Gif
+          main{
+            background: black;
+            width: 100vw;
+            height: 100vh;
+            display: grid;
+            place-items: center;
+          }
+          div {
+            width: 60vw;
+            background: black;
+            display: block;
+            margin: 0 auto;
+          }
+        `}</style>
+    </main>
+  );
+};
+
+export default Gif;
